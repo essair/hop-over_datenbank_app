3a6e24c906c8c65bc423f003d44a06059e30d5fd
essair <radford@oekofor.de>
2021-12-08 17:03:28 +0100

    reduced complexity to see whats going on, add these back in again and remove empty rows from output

--- datatable_attempt_2_testing_filters.R
+++ datatable_attempt_2_testing_filters.R
@@ -19,46 +19,45 @@ ui <-  fluidPage(
   titlePanel("trying out datatable to show selection!! "),
   
   # Create a new Row in the UI for selectInputs
-  fluidRow(
-    column(2,
-           sliderInput("Länge",
-                       "Länge:",
-                       min=min(hopover_pos$Länge, na.rm=TRUE),
-                       max=max(hopover_pos$Länge, na.rm=TRUE),
-                       round=TRUE,
-                       value= c(100, max(hopover_pos$Länge, na.rm=TRUE))
-           )
-    ),
-    column(2,
-           sliderInput("Höhe",
-                       "minimal Höhe:",
-                       min=min(hopover_pos$Höhe, na.rm=TRUE),
-                       max=max(hopover_pos$Höhe, na.rm=TRUE),
-                       step=1,
-                       round=TRUE,
-                       value= 4)
-    ),
-    column(2,
-           selectInput("Strassentyp",
-                       "Straßentyp:",
-                       choices = c("Wähle" = "", levels(hopover_pos$Strassentyp)), multiple = TRUE, selected=unique(levels(hopover_pos$Strassentyp)))
-    ),
-    column(2,
-            sliderInput("Anzahl_spuren",
-                        "Anzahl Spuren:",
-                        step=2,
-                        ticks=TRUE,
-                        min=min(hopover_pos$Anzahl_spuren, na.rm=TRUE),
-                        max=max(hopover_pos$Anzahl_spuren, na.rm=TRUE),
-                        value= 4)
-    )
-    ),
+  # fluidRow(
+  #   column(2,
+  #          sliderInput("Länge",
+  #                      "Länge:",
+  #                      min=min(hopover_pos$Länge, na.rm=TRUE),
+  #                      max=max(hopover_pos$Länge, na.rm=TRUE),
+  #                      round=TRUE,
+  #                      value= c(100, max(hopover_pos$Länge, na.rm=TRUE))
+  #          )
+  #   ),
+  #   column(2,
+  #          sliderInput("Höhe",
+  #                      "minimal Höhe:",
+  #                      min=min(hopover_pos$Höhe, na.rm=TRUE),
+  #                      max=max(hopover_pos$Höhe, na.rm=TRUE),
+  #                      step=1,
+  #                      round=TRUE,
+  #                      value= 4)
+  #   ),
+  #   column(2,
+  #          selectInput("Strassentyp",
+  #                      "Straßentyp:",
+  #                      choices = c("Wähle" = "", levels(hopover_pos$Strassentyp)), multiple = TRUE, selected=unique(levels(hopover_pos$Strassentyp)))
+  #   ),
+  #   column(2,
+  #           sliderInput("Anzahl_spuren",
+  #                       "Anzahl Spuren:",
+  #                       step=2,
+  #                       ticks=TRUE,
+  #                       min=min(hopover_pos$Anzahl_spuren, na.rm=TRUE),
+  #                       max=max(hopover_pos$Anzahl_spuren, na.rm=TRUE),
+  #                       value= 4)
+  #   )
+  #   ),
     fluidRow(
       column(2,
              selectInput("Art",
                          "Art der Struktur:",
-                         c("All",
-                           unique(as.character(hopover_pos$Art))))
+                         choices = c("Wähle" = "", levels(hopover_pos$Art)), multiple = TRUE, selected=unique(levels(hopover_pos$Art)))
       ),
       column(2,
              selectInput("beidseitige_HopOver",
@@ -82,24 +81,30 @@ server <- function(input, output) {
   # Filter data based on selections
   output$table <- DT::renderDataTable(DT::datatable({
     data <- hopover_pos
-    data <- data[data$Länge >=input$Länge[1] & data$Länge <=input$Länge[2] & !is.na(data$Länge), ]
-    data <- data[data$Höhe >= input$Höhe[1], ]
-    if (input$Strassentyp != "All") {
-      data <- data[data$Strassentyp == input$Strassentyp,]
-    }
-    data <- data[data$Anzahl_spuren == input$Anzahl_spuren[1],]
-    }
+    # data <- data[data$Länge >=input$Länge[1] & data$Länge <=input$Länge[2] & !is.na(data$Länge), ]
+    # message(Länge)
+    # print(input$Länge)
+    # data <- data[data$Höhe >= input$Höhe[1], ]
+    # message(Höhe)
+    # print(input$Höhe)
+    # if (input$Strassentyp != "All") {
+    #   data <- data[data$Strassentyp == input$Strassentyp,]
+    # }
+    # message(Strassentyp)
+    # print(input$Strassentyp)
+    # data <- data[data$Anzahl_spuren == input$Anzahl_spuren[1],]
+    # }
     if (input$Art != "All") {
-      data <- data[data$Art == input$Art,]
+     data <- data[data$Art == input$Art,]
     }
     if (input$beidseitige_HopOver != "All") {
-      data <- data[data$beidseitige_HopOver == input$beidseitige_HopOver,]
-    }
+     data <- data[data$beidseitige_HopOver == input$beidseitige_HopOver,]
+   }
     if (input$Gewässerunterführung != "All") {
       data <- data[data$Gewässerunterführung == input$Gewässerunterführung,]
-    }
-    data [, c(-2, -3)]
-  }))
+   }
+  data [, c(-0, -2, -3)]
+}, rownames=FALSE))
   
 }
 
